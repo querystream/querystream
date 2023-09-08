96e7c3bfb33c66efb8311283d7941c12859ffd10
Archie L. Cobbs <archie.cobbs@gmail.com>
2023-09-08 09:53:22 -0500

    Regenerate Maven site.

---
+++ site/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.dellroad.querystream.jpa"},{"l":"org.dellroad.querystream.jpa.querytype"},{"l":"org.dellroad.querystream.jpa.util"},{"l":"org.dellroad.querystream.test.io"},{"l":"org.dellroad.querystream.test.jpa"}];updateSearchResults();
